2557333cffed4933b0a51077075ce320ea82474a
Arold Mauger <aroldmauger2022@gmail.com>
2024-10-17 16:05:37 +0200

    Création class DBConnect et test de la connexion à la BDD en local

---
+++ DBConnect.php
@@ -0,0 +1,8 @@
+<?php
+
+class DBConnect
+{
+    public static function getPDO() {
+        return new PDO("mysql:dbname=database;host=localhost;port=3307", 'user', 'password');
+    }
+}
--- main.php
+++ main.php
@@ -1,11 +1,23 @@
 <?php
+require_once 'DBConnect.php';
 
 while(true) {
     $line = readline("Entrez votre commande : ");
 
     if($line === "list") {
         echo "Affichage de la liste";
-    } else {
+    }
+    elseif($line === "dbtest") {
+        try{
+            $dbconnect = new DBConnect();
+            $pdo = $dbconnect->getPDO();
+
+            echo "Connexion réussie!";
+        } catch (PDOException $e) {
+            echo "La connexion a échoué" . $e->getMessage();
+        }
+    }
+    else {
         echo "Vous avez saisi : $line\n";
     }
 }
