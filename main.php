282ca1dc11a4b22f8d0d9a002d2f769d7b927838
Arold Mauger <aroldmauger2022@gmail.com>
2024-10-17 15:45:39 +0200

    Premier commit, premier script d'affichage

---
+++ main.php
@@ -0,0 +1,11 @@
+<?php
+
+while(true) {
+    $line = readline("Entrez votre commande : ");
+
+    if($line === "list") {
+        echo "Affichage de la liste";
+    } else {
+        echo "Vous avez saisi : $line\n";
+    }
+}
